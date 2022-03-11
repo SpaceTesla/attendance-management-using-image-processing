c26092d8a062df10eeefe5c24ac2c1438c0d2748
penguinacorn <97155550+penguinacorn@users.noreply.github.com>
2022-03-11 15:51:02 +0530

    Update excel_manipulation.py

--- python/excel_manipulation.py
+++ python/excel_manipulation.py
@@ -1,9 +1,9 @@
 # Attendace Checking
 
 
-from openpyxl import load_workbook
-from openpyxl.styles import PatternFill
-from datetime import date
+from openpyxl import load_workbook         #importing openpyxl(module used to work on excel sheets)
+from openpyxl.styles import PatternFill           #option to colour
+from datetime import date                       #importing date and time
 
 # -------
 
@@ -14,9 +14,9 @@ wb = load_workbook(f_path)        # To load the workbook in the path
 
 Date = date.today()               # To fetch today's date
 
-# To choose an active worksheet
-ws = wb.active
-ws.title = str(Date)  # To rename the sheet to the date
+   
+ws = wb.active           # To choose an active worksheet   
+ws.title = str(Date)           # To rename the sheet to the date
 
 col = 2  # 1st row is Heading
 
@@ -28,7 +28,7 @@ j=1
 for i in range(1,6):
     for j in range (1,8):
         try:
-            globals() [f'text.{i}.{j}'] = open(f'text.{i}.{j}.txt', 'r')
+            globals() [f'text.{i}.{j}'] = open(f'text.{i}.{j}.txt', 'r')          #TEXT FILE HAVING NAMES OF ALL STUDENTS
             globals() [f'var{i}.{j}'] = (globals() [f'text.{i}.{j}']).read()
             l.append(globals() [f'var{i}.{j}'])
         except FileNotFoundError:
@@ -43,16 +43,16 @@ max_rows = ws.max_row
 
 for i in range(max_rows):
 
-    element = ws["A" + str(col)].value  # A1 A2 A3 .....
+    element = ws["A" + str(col)].value  # A1 A2 A3 .....(column names)
 
     if element != None:
         if element.lower() in data_str.lower():        # presert condition
             ws["B" + str(col)] = "P"
-            ws["B" + str(col)].fill = PatternFill("solid", fgColor="71FF33")
+            ws["B" + str(col)].fill = PatternFill("solid", fgColor="71FF33")    #colouring the sheet with green beside respective name
 
         else:                          # Abscent condition
             ws["B" + str(col)] = "A"   # To mark absent
-            ws["B" + str(col)].fill = PatternFill("solid", fgColor="F50707")
+            ws["B" + str(col)].fill = PatternFill("solid", fgColor="F50707")      #to colour red
         col += 1
 
 
