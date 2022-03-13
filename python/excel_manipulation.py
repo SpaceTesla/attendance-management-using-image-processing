1abe3fdd9fce56f0bae334210ef7da16f4b6186c
penguinacorn <97155550+penguinacorn@users.noreply.github.com>
2022-03-13 20:01:52 +0530

    Update excel_manipulation.py

--- python/excel_manipulation.py
+++ python/excel_manipulation.py
@@ -1,15 +1,22 @@
 # Attendace Checking
 
 
-from openpyxl import load_workbook         #importing openpyxl(module used to work on excel sheets)
-from openpyxl.styles import PatternFill           #option to colour
-from datetime import date                       #importing date and time
+   #importing openpyxl(module used to work on excel sheets)
+from openpyxl import load_workbook     
+   #importing option to colour
+from openpyxl.styles import PatternFill   
+   #importing date and time
+from datetime import date                    
 
-# -------
+# -------------------------------------------------------------
 
 f = open("text/excel_paths.txt")  # To get the path of the file
+
+
 f_path = f.read()
-wb = load_workbook(f_path)        # To load the workbook in the path
+
+   # Now to load the workbook in the path
+wb = load_workbook(f_path)        
 
 
 Date = date.today()               # To fetch today's date
@@ -33,14 +40,18 @@ for i in range(1,6):
             l.append(globals() [f'var{i}.{j}'])
         except FileNotFoundError:
             pass
+
+print()
 print("no errors")
+print()
 
 for i in l:
     data_str += i
 
-
+#----------------------------------------------------------------------------------------
 max_rows = ws.max_row
 
+   #to traverse the rows in the excel file
 for i in range(max_rows):
 
     element = ws["A" + str(col)].value  # A1 A2 A3 .....(column names)
@@ -48,12 +59,15 @@ for i in range(max_rows):
     if element != None:
         if element.lower() in data_str.lower():        # presert condition
             ws["B" + str(col)] = "P"
-            ws["B" + str(col)].fill = PatternFill("solid", fgColor="71FF33")    #colouring the sheet with green beside respective name
+            #colouring the sheet with green beside respective name
+            ws["B" + str(col)].fill = PatternFill("solid", fgColor="71FF33")    
 
-        else:                          # Abscent condition
-            ws["B" + str(col)] = "A"   # To mark absent
-            ws["B" + str(col)].fill = PatternFill("solid", fgColor="F50707")      #to colour red
+        
+      else:                          # Abscent condition
+            ws["B" + str(col)] = "A"   
+            # To mark absentees with the colour red
+            ws["B" + str(col)].fill = PatternFill("solid", fgColor="F50707")      
         col += 1
 
-
-wb.save(f_path)    # To save all the changes
+    # Now to save all the changes
+wb.save(f_path)   
